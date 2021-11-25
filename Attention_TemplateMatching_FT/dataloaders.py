57a3dc59c5a829b24d1b07b5b55596f21884c038
kaaeaate <ekaterina.radionova@skoltech.ru>
2021-11-25 16:11:00 +0300

    Added MNIST dataloder

--- Attention_TemplateMatching_FT/dataloaders.py
+++ Attention_TemplateMatching_FT/dataloaders.py
@@ -199,3 +199,37 @@ class OneImageDataset(Dataset):
 #             item_image = self.img_transform(item_image)
 
         return item_image
+
+
+class MNISTDataset(Dataset):
+    def __init__(self, images_folder, masks_folder, 
+                 img_transform=None, masks_transform=None):
+        super(Dataset, self).__init__()
+        
+        self.images_folder = images_folder
+        self.masks_folder = masks_folder
+
+        self.images_names = np.sort(os.listdir(images_folder))#[::5] 
+        self.masks_names = np.sort(os.listdir(masks_folder))#[::5]  
+        
+        self.img_transform = img_transform
+        self.masks_transform = masks_transform
+
+    def __len__(self):
+        return len(self.images_names)
+    
+    def __getitem__(self, idx):
+        item_image = np.load(os.path.join(self.images_folder,
+                                            self.images_names[idx]))
+        item_mask = np.load(os.path.join(self.masks_folder,
+                                              self.masks_names[idx]))
+        
+        SEED = np.random.randint(123456789)
+        if self.img_transform is not None:
+            random.seed(SEED)
+            item_image = self.img_transform(item_image)
+        if self.masks_transform is not None:  
+            random.seed(SEED)
+            item_mask = self.masks_transform(item_mask)
+
+        return item_image, item_mask
